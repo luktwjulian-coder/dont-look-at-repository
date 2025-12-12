0e69ff62d6b18d375cc10306be09d3917f0742e5
luktwjulian-coder <luktwjulian@gmail.com>
2025-12-12 17:01:43 +0800

    Add files via upload

---
+++ indes.js
@@ -0,0 +1,33 @@
+document.getElementById("one").addEventListener("click", redirect);
+function redirect(){
+	window.location.href = "qatar.html";
+}
+
+document.getElementById("two").addEventListener("click", redirect1);
+function redirect1(){
+	window.location.href = "SQ.html";
+}
+
+document.getElementById("three").addEventListener("click", redirect2);
+function redirect2(){
+	window.location.href = "CX.html";
+}
+
+document.getElementById("four").addEventListener("click", redirect3);
+function redirect3(){
+	window.location.href = "EK.html";
+
+}
+
+document.getElementById("five").addEventListener("click", redirect4);
+function redirect4(){
+	window.location.href = "AC.html";
+}
+
+document.getElementById("six").addEventListener("click", redirect5);
+function redirect5(){
+	window.location.href = "ANA.html";
+}
+
+
+
